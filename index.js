b5a398d3e909e7d155bd71ad775bc8387b527df3
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-16 19:56:54 -0600

    Creacion de servidor con EXPRESS, puerto 4000

--- index.js
+++ index.js
@@ -3,3 +3,18 @@
 
 //importar express forma actual
 import express from "express";
+
+//llamar la funcion de EXPRESS
+const app = express();
+
+//.USE(), es como EXPRESS maneja el routing(rutas)
+//req = es lo que estoy enviando
+//res = lo que estoy recibiendo
+app.use('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
+//puerto de conexion
+app.listen(4000, () => {
+    console.log("Servidor funcionando en el puerto 4000");
+});
