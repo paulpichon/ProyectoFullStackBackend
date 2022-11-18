696682b516849f870dec43424737ae39e8722ca2
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-17 18:16:05 -0600

    Prevenir registro de usuarios duplicados

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -4,8 +4,22 @@ import Veterinario from "../models/Veterinario.js";
 //registrar
 const registrar = async (req,res) => {
     //destructuring
-    //const {nombre, email, password} = req.body;
+    const {email} = req.body;
+
+    //prevenir usuarios duplicados
+    //busca una coincidencia findOne({email})
+    //en este caso se para como objeto lo que se va a buscar
+    //findOne({email}) = findOne({email:email})
+    const existeUsuario = await Veterinario.findOne({email});
     
+    //si existe usuario
+    if (existeUsuario) {
+        //existe el usuario
+        const error = new Error('Usuario ya registrado');
+        //return para ya no ejecutar las siguientes lineas
+        return res.status(400).json({msg: error.message});
+    }
+
     //trycatch en caso de haber algun error a la hora de registrar 
     try {
         //guardar un nuevo veterinario
