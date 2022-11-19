1f5e3ab93664cbe28ea0ea7374d8488d61f29ebd
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-18 18:04:25 -0600

    Hashear passwords de los usuarios con bcrypt dependencia

--- models/Veterinario.js
+++ models/Veterinario.js
@@ -1,5 +1,7 @@
 //importar moongose
 import mongoose from "mongoose";
+//hashear una contraseña
+import bcrypt, { genSalt } from 'bcrypt';
 //importar generarID
 import generarId from "../helpers/generarId.js";
 
@@ -38,6 +40,22 @@ const veterinarioSchema = mongoose.Schema({
         default: false
     }
 });
+
+//hashear contraseña antes de almacenar en la base de datos
+veterinarioSchema.pre('save', async function(next) {
+    //si esta hasheado no lo vuelva a hashear
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    //crear un salt
+    const salt = await bcrypt.genSalt(10);
+    //reescribir password por el hasheado
+    this.password = await bcrypt.hash(this.password, salt);
+
+});
+
+
 
 //registrar en mongo, se pasa como segundo parametro veterinarioSchema
 const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
