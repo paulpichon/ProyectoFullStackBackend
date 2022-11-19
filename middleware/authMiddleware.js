a3973c29af84d87a0389f2d29eb4e15997229654
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-18 20:17:26 -0600

    comprobando el token en el middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,17 @@
 //revisar autenticacion para el control de paginas que puede o no ver un usuario
 const checkAuth = (req, res, next) => {
-    console.log("desde mi middleware");
+    
+    //comprobando que se envie un token y al mismo tiempo un startsWith('Bearer')
+    if (
+        req.headers.authorization && 
+        req.headers.authorization.startsWith('Bearer') 
+    ) {
+        console.log("si tiene el token con bearer");   
+    }
+
+    //en caso de que no haya token
+    const error = new Error('Token no válido o inexistente');
+    res.status(403).json({msg: error.message});
 
     next();
 };
