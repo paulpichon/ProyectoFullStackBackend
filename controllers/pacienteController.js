1605c1c78a5d776041af42735c28ff1ab00d1a47
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-23 18:56:38 -0600

    Actualizar pacientes, eliminar pacientes

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -29,4 +29,96 @@ const obtenerPacientes = async (req, res) => {
 
 }
 
-export {agregarPaciente, obtenerPacientes};
+//obtener un solo paciente
+const obtenerPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    //si no hay paciente
+    if (!paciente) {
+        return res.status(404).json({msg: "No encontrado"});
+    }
+
+    //verificar si el veterinario que quiere obtener el paciente es el mismo que lo creo
+    //se deben converit en string para que no sean Object.Id
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString() ) {
+        //mostrar mensaje de error
+        return res.json({msg: "Accion no valida"});
+    }
+
+    //si hay paciente
+    res.json(paciente);
+
+}
+//actualizar un solo paciente
+const actualizarPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    //si no hay paciente
+    if (!paciente) {
+        return res.status(404).json({msg: "No encontrado"});
+    }
+
+    //verificar si el veterinario que quiere obtener el paciente es el mismo que lo creo
+    //se deben converit en string para que no sean Object.Id
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString() ) {
+        //mostrar mensaje de error
+        return res.json({msg: "Accion no valida"});
+    }
+
+    //actualizar PACIENTE
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado);
+
+    } catch (error) {
+        console.log( error );
+    }
+
+}
+//eliminar paciente
+const eliminarPaciente = async (req, res) => {
+    //verificar que quien quiere eliminar un registro es quien lo creo
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    //si no hay paciente
+    if (!paciente) {
+        return res.status(404).json({msg: "No encontrado"});
+    }
+
+    //verificar si el veterinario que quiere obtener el paciente es el mismo que lo creo
+    //se deben converit en string para que no sean Object.Id
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString() ) {
+        //mostrar mensaje de error
+        return res.json({msg: "Accion no valida"});
+    }
+
+    try {
+        
+        await paciente.deleteOne();
+        res.json({msg: "Paciente eliminado"});
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+
+}
+
+
+export {
+    agregarPaciente, 
+    obtenerPacientes, 
+    obtenerPaciente,
+    actualizarPaciente,
+    eliminarPaciente
+};
--- routes/pacienteRoutes.js
+++ routes/pacienteRoutes.js
@@ -5,17 +5,29 @@ const router = express.Router();
 //importar agregaPaciente y obtenerPacientes
 import {
     agregarPaciente, 
-    obtenerPacientes
+    obtenerPacientes, 
+    obtenerPaciente,
+    actualizarPaciente,
+    eliminarPaciente
     } from '../controllers/pacienteController.js';
 //importar checkAuth
 import checkAuth from '../middleware/authMiddleware.js';
 //protegemos agregarPaciente con checkAuth
 //para agregar un paciente debes tener una cuenta
 //para obtener pacientes debes tener checkuth
+//obtener todos los PACIENTES
 router
     .route('/')
     .post(checkAuth, agregarPaciente)
     .get(checkAuth, obtenerPacientes)
+
+//obtener un solo PACIENTE
+router
+    .route('/:id')
+    .get(checkAuth, obtenerPaciente)
+    .put(checkAuth, actualizarPaciente)
+    .delete(checkAuth, eliminarPaciente)
+
 
 //exportar
 export default router;
