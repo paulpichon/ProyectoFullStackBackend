461440547c70f5a68d3f38079223d6db00e5633e
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-24 19:36:13 -0600

    Configuracion con SMTP aquiestoy

--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer';
-
+//CONFIGURACION CUANDO SE USA UN SMTP EXTERNO 
+//https://stackoverflow.com/questions/43472705/error-hostname-ip-doesnt-match-certificates-altnames-node-js/45706464#45706464
 const emailRegistro = async (datos) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
@@ -7,7 +8,12 @@ const emailRegistro = async (datos) => {
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        }
+        },
+        secure: true,
+        tls: { 
+            rejectUnauthorized: false 
+        },
+        name: "APV"
     });
 
 
@@ -16,17 +22,17 @@ const emailRegistro = async (datos) => {
 
     //enviar EMAIL
     const info = await transporter.sendMail({
-        from: "APV - ADMINISTADOR DE PACIENTES DE VETERINARIA",
+        from: 'Administrador de Pacientes de Veterianaria',
         to: email,
         subject: 'Comprueba tu cuenta en APV',
-        text: 'Comprueba tu cuienta en APV',
+        text: 'Comprueba tu cuenta en APV',
         html: `<p>Hola: ${nombre}, comprueba tu cuenta en APV.</p>
                 <p>Tu cuenta ya esta lista, solo debes comprobarla en el siguiente enlace: 
                     <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a>
                 </p>
                 <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
         
-        `
+        `,
     });
 
     console.log("MENSAJE ENVIADO: %s", info.messageId );
